2c199cb32ae1d2d96d65e23bbf5743653d249b37
arfrhmnshrtn <iw8628940@gmail.com>
2021-07-04 14:45:06 +0700

    merombak

--- script.js
+++ script.js
@@ -1,46 +1,38 @@
-const button = document.querySelectorAll('.btn');
-const input = document.querySelector('.input');
-const input1 = document.querySelector('.input-1');
-const hasil = document.querySelector('.hasil');
+const btn = document.querySelectorAll('.btn');
+const output = document.querySelector('.output');
+const history = document.querySelector('.history');
+const equals = document.querySelector('.equals');
 const clear = document.querySelector('.clear');
-const darkMode = document.querySelector('.dark-mode');
-const container = document.querySelector('.container');
-const salmon = document.querySelectorAll('.salmon');
-const icon = document.querySelector('.icon');
-const btn = document.querySelector('.button');
 
 // looping button
-for (let i = 0; i < button.length; i++){
+for (let i = 0; i < btn.length; i++){
     // jika button index ke-i di click maka akan menambahkan ke dalam value input
-    button[i].addEventListener('click', function(){
+    btn[i].addEventListener('click', function(){
         // let hasil = input.value;
-input.value += button[i].value;
-        input1.value = input.value;
+        output.value += btn[i].value;
+        history.value = output.value;
     });
 }
 
 // event button delete
-button[2].addEventListener('click', function(){
-    let str = String(input.value);
-    let a = str.split('').slice(0, input.value.length - 1).join('');
-    input.value = a;
+btn[2].addEventListener('click', function(){
+    let str = String(output.value);
+    let a = str.split('').slice(0, output.value.length - 1).join('');
+    output.value = a;
 });
 
 // event tombol hasil / =
-hasil.addEventListener('click', function(){
-    if (input.value.length == 0){
-        input.value = '';
+equals.addEventListener('click', function(){
+    if (output.value.length == 0){
+        output.value = '';
     }else{
-        input.value = eval(input.value);
-        input1.style.opacity = '1';
+        output.value = eval(output.value);
     }
-    console.log(input.value);
 });
 
 // event clear all value input
 clear.addEventListener('click', function(){
-    input.value = '';
-    input1.value = '';
+    output.value = '';
+    history.value = '';
     window.location.reload('Refresh');
 });
-
